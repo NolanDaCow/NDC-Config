ea70b18e30afc9d81c3cb5af74d6589e61f6aa88
NolanDaCow <nolanrvg@gmail.com>
2025-06-10 09:51:10 -0500

    Updated theme.toml, install.sh, and README.md

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-sudo pacman -Syuu btop cava hyprland kitty nvim rofi spotify-player sddm waybar
+cd ~/.config
+git clone https://github.com/NolanDaCow/cautious-happiness
+mv ~/.config/cautious-happiness/* .
+rm -r -f .git cautious-happiness
+sudo pacman -Syuu btop cava hyprland kitty nvim rofi spotify-player sddm waybar pipewire wireplumber swaync qt5-wayland qt6-wayland steam discord
+yay -Syuu visual-studio-code-bin
