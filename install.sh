858654165ce9769b08986a76dcc8ff6b1595b4a3
Nolan <124066879+NolanDaCow@users.noreply.github.com>
2025-06-10 09:13:38 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo pacman -Syuu btop cava hyprland kitty nvim rofi spotify-player sddm waybar
+sudo pacman -Syuu btop cava hyprland kitty nvim rofi spotify-player sddm waybar pipewire wireplumber swaync qt5-wayland qt6-wayland steam discord
+yay -Syuu visual-studio-code-bin
