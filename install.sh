c5060efb61af8b2feff152f7b1babc00d1d7e06b
NolanDaCow <nolanrvg@gmail.com>
2025-06-17 09:47:25 -0500

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd ~/.config
+sudo chmod -R +rw ~/.config/cautious-happiness/*
+sudo chown $USER ~/.config/cautious-happiness/*
 sudo mv ~/.config/cautious-happiness/* .
 sudo rm -r -f .git cautious-happiness
 sudo pacman -Syuu btop cava hyprland hyprpaper kitty nemo cowsay figlet nvim rofi spotify-player sddm waybar pipewire wireplumber swaync qt5-wayland qt6-wayland steam discord qt5-quickcontrols2 qt5-graphicaleffects qt5-svg steam discord otf-font-awesome
@@ -8,3 +10,5 @@ yay -Syuu visual-studio-code-bin hyprshot
 sudo mv ~/.config/tokyo-night-sddm /usr/share/sddm/themes/
 #HYPR SETUP
 sudo chmod +x ~/.config/hypr/scripts/*
+sudo chmod +x ~/.config/waybar/scripts/*
+systemctl enable sddm
